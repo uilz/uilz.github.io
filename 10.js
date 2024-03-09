6ef4115e12169420da9d6d0a858681125d25e2b8
Mr.Liu <121709287+uilz@users.noreply.github.com>
2024-03-09 20:37:35 +0800

    优化网站结构

---
+++ 10.js
@@ -0,0 +1,38 @@
+var names = ["刘健翔","庞金帅","林晓洁","梁雅诗","谢斌锋","张琳欣","杨博","谢晓","杨哲聪","徐麒","彭威","周丹阳","梁志武","谢嘉兴","曾令轩","曾嘉瑜","吴海汪","施展谋","陈焕","向美芯","汤蕾","郑希怡","乔梓健","钟博文","文小杰","谭智豪","张子欣","胡俊权","朱奕濠","林淑雯","龚小于","李键朗","谭丽萱","郭广圣","尹晓敏","叶俊熙","骆俐桥","何梓轩","胡浩群","唐贵旭","余俊豪","陈贝琳","陈烁锋","王旭斌","周彦文","陈栎轩","吴桐雨","吴一卓","游宁都","廖涛","陈颢中","陈浩斌","李光栩","彭根裕","谢宇"];
+var length = names.length;
+
+var displayBoard = document.getElementById("nameDisplay");
+
+mytime = null
+
+function clickButton(btn) {
+    var text = btn.innerHTML;
+    console.log(text);
+
+    if (text == "启动!") {
+        btn.innerHTML = "抽取幸运儿!";
+
+        if (mytime == null) {
+            mytime = setInterval(function () {
+                var name = randomName();
+                console.log(name);
+                displayBoard.innerHTML = name;
+            }, 1);
+        }
+
+    } else if (text == "抽取幸运儿!") {
+        btn.innerHTML = "启动!";
+
+        if (mytime != null) {
+            clearInterval(mytime);
+            mytime = null;
+        }
+    }
+
+
+}
+
+function randomName() {
+    var randomNum = Math.ceil(Math.random() * 54) % length + 1;
+    return names[randomNum - 1];
+}
---
+++ 9/09.js
@@ -0,0 +1,39 @@
+
+var names = ["张海丰","唐可儿","陈海玉","刘文珍","张敏玲","卢鹏","陶子萱","罗广","李浩","盛浩峰","陈卓","廖宇阳","李梓里","叶金娜","郑宇胜","邓凯尹","林炳锋","李瑞奇","郭浩鹏","李乐瑶","吴佳欣","程奕涛","李佳玲","黄冠铭","甘亦超","王米柔","刘韬","何穗希","陈锡涛","邓子轩","叶伟杰","梁说恩","林佳琪","陈俊宇","钟欣彤","沈玮宗","余圳威","胡静雯","黄海凝","杨颖德","陈嘉豪","张和","何冠希","李子晴","何冠杰","龙周天伊","陈科昕","林文豪","马永淳","王恒书","陈朗","杨光","钟汶芯","刘俊彦","杨杰"];
+var length = names.length;
+
+var displayBoard = document.getElementById("nameDisplay");
+
+mytime = null
+
+function clickButton(btn) {
+    var text = btn.innerHTML;
+    console.log(text);
+
+    if (text == "启动!") {
+        btn.innerHTML = "抽取幸运儿!";
+
+        if (mytime == null) {
+            mytime = setInterval(function () {
+                var name = randomName();
+                console.log(name);
+                displayBoard.innerHTML = name;
+            }, 1);
+        }
+
+    } else if (text == "抽取幸运儿!") {
+        btn.innerHTML = "启动!";
+
+        if (mytime != null) {
+            clearInterval(mytime);
+            mytime = null;
+        }
+    }
+
+
+}
+
+function randomName() {
+    var randomNum = Math.ceil(Math.random() * 54) % length + 1;
+    return names[randomNum - 1];
+}
